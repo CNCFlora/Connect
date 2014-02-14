e4625f3aa7a83d6b6f297a027354533fad47d688
Diogo Souza <diogo@diogok.net>
2014-02-14 15:56:48 -0200

    fixing vagrant bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,17 +7,18 @@ apt-get install openjdk-7-jdk curl git tmux vim htop -y
 # install leiningen
 [[ ! -e lein ]] && wget https://raw.github.com/technomancy/leiningen/stable/bin/lein -O /usr/bin/lein
 chmod +x /usr/bin/lein
+su vagrant -c 'lein self-install'
 
 # create db dir
 [[ ! -e /var/lib/floraconnect ]] && mkdir -p /var/lib/floraconnect
 chown vagrant /var/lib/floraconnect -Rf
 
 # prepare startup
-echo 'echo $(date) > /var/log/rc.log' > /etc/rc.log
-echo 'su -c "cd /vagrant && nohup lein ring server-headless &" >> /var/log/rc.log 2>&1' >> /etc/rc.local
+echo 'echo $(date) > /var/log/rc.log' > /etc/rc.local
+echo 'su vagrant -c "cd /vagrant && nohup lein ring server-headless &" >> /var/log/rc.log 2>&1' >> /etc/rc.local
 echo 'SUBSYSTEM=="bdi",ACTION=="add",RUN+="/vagrant/register.sh >> /var/log/rc.log 2>&1"' > /etc/udev/rules.d/50-vagrant.rules
 
 # start the service and register it
-cd /vagrant && su vagrant -c 'nohup lein ring server-headless &'
-cd /vagrant && ./register.sh
+su vagrant -c 'cd /vagrant && nohup lein ring server-headless &'
+cd /vagrant && chmod +x register.sh && ./register.sh
 
