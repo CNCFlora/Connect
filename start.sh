afbe122627274ab7e08877ba80e5e8e7b15526e7
Diogo Souza <diogo@diogok.net>
2014-04-29 13:13:03 -0300

    improving docker

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+chown $APP_USER /var/lib/floraconnect -Rf
+
 [[ ! $CONTEXT ]] && CONTEXT="/"
 su $APP_USER -c "cd ~/ && nohup java -jar jetty.jar --path $CONTEXT connect.war > connect.log 2>&1 &" &
 
