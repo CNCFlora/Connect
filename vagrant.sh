c1721fc76685ac0daf69641a79e591de619ec6d0
Diogo Souza <diogo@diogok.net>
2014-05-13 15:02:11 -0300

    no need for docker

--- vagrant.sh
+++ vagrant.sh
@@ -13,9 +13,3 @@ su vagrant -c 'lein self-install'
 [[ ! -e /var/lib/floraconnect ]] && mkdir -p /var/lib/floraconnect
 chown vagrant /var/lib/floraconnect -Rf
 
-# docker, to build the image
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-echo 'deb http://get.docker.io/ubuntu docker main' > /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get install lxc-docker
-
