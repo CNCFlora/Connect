65c6f455778f4bc296df333a4b9328663089b099
Diogo Souza <diogo@diogok.net>
2014-07-09 13:24:05 -0300

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject flora-connect "0.2.0"
+(defproject flora-connect "0.2.1"
   :description "Single Sign On for the CNCFlora systems"
   :url "http://github.com/CNCFlora/connect"
   :main flora-connect.server
